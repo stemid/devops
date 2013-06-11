83e2ac26c21e6a8f0335bed59cc752ec5a91a6aa
swehack <stefan.midjich@cygate.se>
2013-06-11 10:20:11 +0200

    Colout solarized theme for nagios logs.

--- nagios/colout_nagios.py
+++ nagios/colout_nagios.py
@@ -7,21 +7,26 @@
 # By Stefan Midjich
 
 def theme():
+    # Solarized specific colors
+    bright_red = '#d75f00'
     red = '#dc322f'
     yellow = '#b58900'
     green = '#859900'
+    bright_white = '#ffffd7'
     white = '#e4e4e4'
     cyan = '#2aa198'
+
     return [
         [ "(SERVICE ALERT\:) (.+)(;OK;HARD;)(.+)$", green ],
         [ "(SERVICE ALERT\:) (.+)(;OK;SOFT;)(.+)$", "#2aa198" ],
-        [ "(SERVICE ALERT\:) (.+;)(UNKNOWN|CRITICAL)(;HARD;)(.+)$", red ],
+        [ "(SERVICE ALERT\:) (.+;)(UNKNOWN|CRITICAL)(;HARD;)(.+)$", bright_red ],
         [ "(SERVICE ALERT\:) (.+;)(UNKNOWN|CRITICAL)(;SOFT;)(.+)$", yellow ],
         [ "(SERVICE ALERT\:) (.+)(;WARNING;HARD;)(.+)$", red ],
         [ "(SERVICE ALERT\:) (.+)(;WARNING;SOFT;)(.+)$", yellow ],
         [ "(HOST ALERT\:) (.+)(;UP;HARD;)(.+)$", green ],
         [ "(HOST ALERT\:) (.+)(;UP;SOFT;)(.+)$", cyan ],
-        [ "(HOST ALERT\:) (.+)(;DOWN;HARD;)(.+)$", red ],
+        [ "(HOST ALERT\:) (.+)(;DOWN;HARD;)(.+)$", bright_red ],
         [ "(HOST ALERT\:) (.+)(;DOWN;SOFT;)(.+)$", yellow ],
         [ "(EXTERNAL COMMAND\:) (.+)$", white, "bold" ],
+        [ "(Finished daemonizing.+)$", bright_white ],
     ]
