0a04347dd550f094be6a955483ddcb4a06ce240d
swehack <stefan.midjich@cygate.se>
2013-06-11 10:17:34 +0200

    Colout solarized theme for nagios logs.

--- nagios/colout_nagios.py
+++ nagios/colout_nagios.py
@@ -1,5 +1,5 @@
 # Solarized colout theme for nagios logs
-#  * Install colout from pypi or https://github.com/nojhan/colout
+#  * Install colout from https://github.com/nojhan/colout
 #  * mkdir ~/.colout
 #  * Install this file into ~/.colout/colout_nagios.py
 #  * tail -0f /var/log/nagios3/nagios.log | perl -pi -e 's/(\d+)/localtime($1)/e' | colout -T ~/.colout -t nagios
@@ -7,16 +7,21 @@
 # By Stefan Midjich
 
 def theme():
+    red = '#dc322f'
+    yellow = '#b58900'
+    green = '#859900'
+    white = '#e4e4e4'
+    cyan = '#2aa198'
     return [
-        [ "(SERVICE ALERT\:) (.+)(;OK;HARD;)(.+)$", "#859900" ],
+        [ "(SERVICE ALERT\:) (.+)(;OK;HARD;)(.+)$", green ],
         [ "(SERVICE ALERT\:) (.+)(;OK;SOFT;)(.+)$", "#2aa198" ],
-        [ "(SERVICE ALERT\:) (.+;)(UNKNOWN|CRITICAL)(;HARD;)(.+)$", "#dc322f" ],
-        [ "(SERVICE ALERT\:) (.+;)(UNKNOWN|CRITICAL)(;SOFT;)(.+)$", "#b58900" ],
-        [ "(SERVICE ALERT\:) (.+)(;WARNING;HARD;)(.+)$", "#dc322f" ],
-        [ "(SERVICE ALERT\:) (.+)(;WARNING;SOFT;)(.+)$", "#b58900" ],
-        [ "(HOST ALERT\:) (.+)(;UP;HARD;)(.+)$", "#859900" ],
-        [ "(HOST ALERT\:) (.+)(;UP;SOFT;)(.+)$", "#2aa198" ],
-        [ "(HOST ALERT\:) (.+)(;DOWN;HARD;)(.+)$", "#dc322f" ],
-        [ "(HOST ALERT\:) (.+)(;DOWN;SOFT;)(.+)$", "#b58900" ],
-        [ "(EXTERNAL COMMAND\:) (.+)$", "#e4e4e4", "bold" ],
+        [ "(SERVICE ALERT\:) (.+;)(UNKNOWN|CRITICAL)(;HARD;)(.+)$", red ],
+        [ "(SERVICE ALERT\:) (.+;)(UNKNOWN|CRITICAL)(;SOFT;)(.+)$", yellow ],
+        [ "(SERVICE ALERT\:) (.+)(;WARNING;HARD;)(.+)$", red ],
+        [ "(SERVICE ALERT\:) (.+)(;WARNING;SOFT;)(.+)$", yellow ],
+        [ "(HOST ALERT\:) (.+)(;UP;HARD;)(.+)$", green ],
+        [ "(HOST ALERT\:) (.+)(;UP;SOFT;)(.+)$", cyan ],
+        [ "(HOST ALERT\:) (.+)(;DOWN;HARD;)(.+)$", red ],
+        [ "(HOST ALERT\:) (.+)(;DOWN;SOFT;)(.+)$", yellow ],
+        [ "(EXTERNAL COMMAND\:) (.+)$", white, "bold" ],
     ]
