fb420c742098b58e6ef0d81b22caf90bf96ff2d6
Stefan Midjich <swehack@gmail.com>
2013-06-14 10:49:50 +0200

    New version of mysqlbackup with compression and host support.

--- tools/mysqlbackup.sh
+++ tools/mysqlbackup.sh
@@ -1,44 +1,64 @@
 #!/bin/bash
-# Fyll i konfigurationsalternativen här nedan med;
 # * Kundnamn, som används i destinationskatalogens namn
 # * Databasens namn
 # * Databasens användare
 # * Användarens lösenord
 
-customerName="kundnamn"
-mysqlDB="databasnamn"
-mysqlUser="användarnamn"
-mysqlPass="lösenord"
+customerName=""
+mysqlUser=""
+mysqlPass=""
+mysqlDB=""
+mysqlHost=""
+
+# Compress dumps
+gzip=1
 
 # Syntaxet av date(1) kommandot skiljer sig 
 # från Mac OS och BSD. 
-backupsDir="/var/backups/${customerName}"
+backupsDir="/var/db/backups/${customerName}"
 todayString=$(date -d today +%Y%m%d)
 todayStamp=$(date -d today +%s)
 weekAgoStamp=$(date -d 'week ago' +%s)
-verbose=0
+
+dumpCmd="mysqldump"
+
+if [ -n "$mysqlHost" ]; then
+  dumpCmd+=" -h$mysqlHost"
+fi
+
+dumpCmd+=" -u'${mysqlUser}' -p'${mysqlPass}'"
+if [ -z "$mysqlDB" ]; then
+  dumpCmd+=' --all-databases'
+fi
+
+mysqlDB=${mysqlDB:-"all-databases"}
 
 # Skapa backup-katalogen om den inte existerar. 
-if [ ! -d "${backupsDir}" ]; then
-	test $verbose -eq 1 && echo "Creating directory: ${backupsDir}"
-	mkdir -p "${backupsDir}" || exit 1
+if [[ ! -d "${backupsDir}" ]]; then
+  echo "Creating directory: ${backupsDir}"
+  mkdir -p "${backupsDir}" || exit 1
 fi
 
 # Rensa gamla SQL dumpar
 for sqlDump in "${backupsDir}"/${mysqlDB}-*.sql; do
-	# Syntaxet av stat(1) kommandot skiljer sig 
-	# på BSD och Mac OS Unix till exempel. 
-	if [ $(stat -c %Z "${sqlDump}") -lt ${weekAgoStamp} ]; then
-		rm -f ${sqlDump} || exit 1
-	fi
+  # Syntaxet av stat(1) kommandot skiljer sig 
+  # på BSD och Mac OS Unix till exempel. 
+  if [[ $(stat -c %Z "${sqlDump}" >/dev/null 2>&1) -lt ${weekAgoStamp} ]]; then
+    rm -f ${sqlDump} || exit 1
+  fi
 done
 
 todaysMysqlDump="${backupsDir}/${mysqlDB}-${todayString}.sql"
 
 # Avsluta om dagens backup redan existerar. 
-if [ -f "${todaysMysqlDump}" ]; then
-	echo "Todays dump already exists, exiting" 1>&2 && exit 1
+if [[ -f "${todaysMysqlDump}" ]]; then
+  echo "Todays dump already exists, exiting" && exit 1
 fi
 
 # Skapa ny SQL dump
-mysqldump -u"${mysqlUser}" -p"${mysqlPass}" "${mysqlDB}" > "${todaysMysqlDump}" || exit 1
+$dumpCmd "${mysqlDB}" > "${todaysMysqlDump}" || exit 1
+
+if [ $gzip -eq 1 ]; then
+  gzip "${todaysMysqlDump}" && exit 0
+fi
+
