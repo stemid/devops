5b7f7a940560390bd168aeb266804e72fcb0d33c
Stefan Midjich <swehack@gmail.com>
2014-05-17 18:39:46 +0200

    tool that helps when you always want to use sudo

---
+++ tools/sudoadd.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Add things to sudoers easily while executing them the first time. 
+# Only tested on Ubuntu 14.04 so far. 
+# by Stefan Midjich <swehack@gmail.com>
+
+username="$(whoami)"
+sudoers_file="/etc/sudoers.d/sudoadd_$username"
+sudoers_testfile="/tmp/sudoadd_$username.test"
+sudoers_data="$username ALL=NOPASSWD:$*"
+
+sudo echo -n "Adding to $sudoers_file: "
+
+# Test it with visudo first
+echo "$sudoers_data" | sudo tee "$sudoers_testfile"
+sudo visudo -cf "$sudoers_testfile";rc=$?
+sudo rm -f "$sudoers_testfile"
+test $rc -ne 0 && exit $?
+
+# Add it for real
+echo "$sudoers_data" | sudo tee -a "$sudoers_file" &>/dev/null
+
+# Lastly execute command
+sudo $*
