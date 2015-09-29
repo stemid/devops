66044b7682cf8db27a59198b022d70df8b603f71
Stefan Midjich <swehack@gmail.com>
2015-09-29 15:19:02 +0200

    new tool added

---
+++ tools/dump_multipart.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Dissect multipart payloads in emails and dump the message in clear text
+# instead of base64 encoded chunks. 
+# by Stefan Midjich <swehack@gmail.com>
+
+from sys import exit
+from argparse import ArgumentParser
+from email.parser import Parser
+
+
+parser = ArgumentParser()
+
+parser.add_argument(
+    '-f', '--file',
+    type=file,
+    dest='mail_file',
+    help='Input mail file'
+)
+
+args = parser.parse_args()
+
+mail = Parser().parse(args.mail_file)
+if mail.is_multipart is False:
+    exit(1)
+
+payloads = mail.get_payload()
+
+for payload in payloads:
+    print payload.get_payload(decode=True)
