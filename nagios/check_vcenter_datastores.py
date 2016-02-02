5b1563e5acc9591d05fd7e1f04b6fee22a1a0c04
Stefan Midjich <swehack@gmail.com>
2016-02-02 09:33:02 +0100

    implemented IO separation for nagios handling.

--- nagios/check_vcenter_datastores.py
+++ nagios/check_vcenter_datastores.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Nagios monitoring script for vcenter datastores.
-# 
+#
 # by Stefan Midjich 2016
 
 from __future__ import print_function
 
 import atexit
+import math
 from sys import exit, stderr
 from fnmatch import fnmatch
 from argparse import ArgumentParser
 from ConfigParser import ConfigParser
 from pprint import pprint
+from operator import itemgetter as i
+from functools import cmp_to_key
 
 from pyVim.connect import SmartConnect, Disconnect
 from pyVmomi import vim
@@ -58,15 +61,65 @@ parser.add_argument(
 
 parser.add_argument(
     '-W', '--warning',
+    type=int,
     default=90,
+    metavar='%',
     help='Warning threshold in percent of used space.'
 )
 
 parser.add_argument(
     '-C', '--critical',
+    type=int,
     default=95,
+    metavar='%',
     help='Critical threshold in percent of used space.'
 )
+
+parser.add_argument(
+    '-O', '--overcommitted',
+    type=int,
+    default=0,
+    metavar='%',
+    help='Alert if any datastore is overcommitted by this percentage.'
+)
+
+parser.add_argument(
+    '-m', '--max-alerts',
+    type=int,
+    default=4,
+    metavar='NUMBER',
+    help='Number of alerts to show before cutting off output and abbreviating.'
+)
+
+
+def convertSize(size):
+   if (size == 0):
+       return '0B'
+   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size,1024)))
+   p = math.pow(1024,i)
+   s = round(size/p,2)
+   return '%s %s' % (s,size_name[i])
+
+
+def multikeysort(items, columns):
+    comparers = [
+        (
+            (
+                i(col[1:].strip()), -1
+            ) if col.startswith('-') else (i(col.strip()), 1)
+        )
+        for col in columns
+    ]
+
+    def comparer(left, right):
+        comparer_iter = (
+            cmp(fn(left), fn(right)) * mult
+            for fn, mult in comparers
+        )
+        return next((result for result in comparer_iter if result), 0)
+    return sorted(items, key=cmp_to_key(comparer))
+
 
 def get_datastores(si, include, exclude):
     datastores = []
@@ -77,6 +130,7 @@ def get_datastores(si, include, exclude):
                 continue
             if fnmatch(ds.name, include):
                 summary = ds.summary
+                # Convert from bytes to KB so my convertSize function works
                 datastores.append({
                     'name': summary.name,
                     'capacity': float(summary.capacity),
@@ -85,6 +139,68 @@ def get_datastores(si, include, exclude):
                 })
 
     return datastores
+
+
+def print_datastore(ds):
+    print(('{name}: Free {free_gb}[{free}%], '
+           'Used {used_gb}[{used}%], '
+           'Uncommitted {uncommitted_gb}[{uncommitted}%], '
+           'Total {total} (Overcommitted {overcommitted}%)').format(
+               used_gb=convertSize(ds.get('used_bytes')),
+               used=ds.get('used'),
+               overcommitted=ds.get('overcommitted'),
+               free_gb=convertSize(ds.get('freeSpace')),
+               uncommitted_gb=convertSize(ds.get('uncommitted')),
+               name=ds.get('name'),
+               free=ds.get('free'),
+               uncommitted=ds.get('uncommitted_percent'),
+               total=convertSize(ds.get('capacity'))
+           )
+         )
+
+
+# This handles Nagios alerts
+# TODO: Monitorscout handler
+def handle_alerts(ds):
+    args = parser.parse_args()
+
+    overcommitted = next(
+        (x for x in ds if x['overcommitted'] > args.overcommitted)
+    )
+
+    datastores = multikeysort(ds, ['overcommitted', 'used'])
+
+    if datastores[0].get('used') > args.critical:
+        out_msg = 'CRITICAL: '
+        exit_code = 2
+    elif datastores[0].get('used') > args.warning:
+        out_msg = 'WARNING: '
+        exit_code = 1
+    elif datastores[0].get('overcommitted') > args.overcommitted:
+        out_msg = 'WARNING: '
+        exit_code = 1
+    else:
+        out_msg = 'UNKNOWN: '
+        exit_code = 3
+
+    count=0
+    for d in datastores:
+        if count > args.max_alerts:
+            break
+        count += 1
+        if d.get('overcommitted') > args.overcommitted:
+            out_msg += '{name} overcommitted with {perc}% | '.format(
+                name=d.get('name'),
+                perc=d.get('overcommitted')
+            )
+        else:
+            out_msg += '{name} uses {perc}% of space | '.format(
+                name=d.get('name'),
+                perc=d.get('used')
+            )
+
+    print(out_msg)
+    exit(exit_code)
 
 
 def main():
@@ -113,12 +229,12 @@ def main():
         )
     except Exception as e:
         print(
-            'CRITICAL: Could not connect to vcenter server: {0}'.format(
+            'Could not connect to vcenter server: {0}'.format(
                 str(e)
             ),
             file=stderr
         )
-        if args.verbose:
+        if args.verbose > 1:
             raise Exception(e)
         else:
             exit(2)
@@ -128,7 +244,9 @@ def main():
     datastores = []
     datastores = get_datastores(si, include=args.include, exclude=args.exclude)
 
-    # Process list of results
+    alert_ds = []
+
+    # Process dataset of results returned
     for ds in datastores:
         free_percent = int((
             ds.get('freeSpace')/ds.get('capacity')
@@ -137,12 +255,33 @@ def main():
         uncommitted_percent = int((
             ds.get('uncommitted')/ds.get('capacity')
         ) * 100)
-        print('{name}: {free}%, {uncommitted}%, {total}GB'.format(
-            name=ds.get('name'),
-            free=free_percent,
-            uncommitted=uncommitted_percent,
-            total=int(ds.get('capacity'))/1024/1024/1024
-        ))
+        overcommitted_space = ds.get('uncommitted')-ds.get('freeSpace')
+        overcommitted_percent = int((
+            overcommitted_space/ds.get('capacity')
+        ) * 100)
+
+        ds['overcommitted'] = overcommitted_percent
+        ds['used'] = used_percent
+        ds['used_bytes'] = ds.get('capacity')-ds.get('freeSpace')
+        ds['free'] = free_percent
+        ds['uncommitted_percent'] = uncommitted_percent
+
+        if args.verbose > 1:
+            print_datastore(ds)
+
+        if overcommitted_percent > args.overcommitted:
+            alert_ds.append(ds)
+            continue
+
+        if args.warning < used_percent or args.critical < used_percent:
+            alert_ds.append(ds)
+            continue
+
+    if len(alert_ds) and args.verbose > 1:
+        print(alert_ds)
+
+    if len(alert_ds):
+        handle_alerts(alert_ds)
 
 
 if __name__ == '__main__':
