4d78d43a21d330442dcffcdad62972f3e648cc45
Stefan Midjich <swehack@gmail.com>
2016-02-11 16:31:53 +0100

    new tool.

---
+++ tools/search_shares.ps1
@@ -0,0 +1,40 @@
+# Search all shares of a computer for a filename pattern.
+#
+# Example: .\search_shares.ps1 -Filter "*.encrypted" -ServerName fileserver01
+#
+# Optional arguments:
+# -Recurse - Boolean determining whether to recursively search or not
+# -OutputFile - Output search results into CSV format file
+# 
+# by Stefan Midjich <swehack@gmail.com> - 2016
+
+param(
+  [string]$ServerName = "localhost",
+  [string]$OutputFile = $false,
+  [switch]$Recurse = $false,
+  [string]$Filter = $(throw "-Filter is required")
+)
+
+$report = @()
+
+foreach ($share in get-wmiobject -class win32_share -computer $ServerName) {
+  $SearchPath = "\\$($ServerName)\$($share.Name)"
+  Write-Progress -Activity "search_shares.ps1" -Status "Searching $($SearchPath)..."
+
+  foreach ($File in (gci -Recurse:$Recurse -Path:$SearchPath -Filter:$Filter | where {! $_.PSIsContainer})) {
+    $row = "" | select Owner, FullName, CreationTime, LastAccessTime, LastWriteTime
+    $row.Owner = (get-acl $File.FullName).Owner
+    $row.FullName = $File.FullName
+    $row.CreationTime = $File.CreationTime
+    $row.LastAccessTime = $File.LastAccessTime
+    $row.LastWriteTime = $File.LastWriteTime
+
+    $report += $row
+
+    if ($OutputFile -ne $false) {
+      $row | export-csv -Path $OutputFile -Append
+    } else {
+      write-host $row
+    }
+  }
+}
