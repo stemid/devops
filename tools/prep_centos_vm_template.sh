4746f39dd05e6688f28cdd34da6667dfea0e0db5
Stefan Midjich <swehack@gmail.com>
2016-10-13 15:58:43 +0200

    new tool.

---
+++ tools/prep_centos_vm_template.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+# Prepare a CentOS/Fedora VM for templating.
+
+yum -y update
+yum clean all
+
+# Remove SSH host keys, they will re-generate at boot.
+find /etc/ssh/*host* |xargs unlink
+find /root/.ssh/ -type f |xargs unlink
+find /home/*/.ssh/ -type f |xargs unlink
+
+# Replace UUIDs with actual devices in /etc/fstab
+while IFS=$'= \t' read -r -a data; do
+	uuid="${data[1]}"
+	blkid -U "$uuid" &>/dev/null|| continue
+	disk=$(blkid -U "$uuid")
+	test -b "$disk" || continue
+	sed -i -e "s:^UUID=[^\s\t]*:$disk:" /etc/fstab
+done < <(grep ^UUID= /etc/fstab)
+
+# Maybe use this solution instead.
+# for disk_uuid in /dev/disk/by-uuid/*; do
+# 	disk=$(readlink -f $disk_uuid)
+#		uuid=$(basename $disk_uuid)
+#		if grep -c "^UUID=$uuid" /etc/fstab; then
+#			sed -i -e "s:^UUID=$uuid:$disk:" /etc/fstab
+#		fi
+#	done
+
+# Replace UUIDs from network config
+sed -i -e '/^UUID=.*/d' /etc/sysconfig/network-scripts/ifcfg-*
+
+# Remove other network specific config from.
+sed -i -e '/^IPV6_.*/d' /etc/sysconfig/network-scripts/ifcfg-*
+sed -i -e '/^IPADDR=.*/d' /etc/sysconfig/network-scripts/ifcfg-*
+sed -i -e '/^PREFIX=.*/d' /etc/sysconfig/network-scripts/ifcfg-*
+sed -i -e '/^GATEWAY=.*/d' /etc/sysconfig/network-scripts/ifcfg-*
+sed -i -e '/^DOMAIN=.*/d' /etc/sysconfig/network-scripts/ifcfg-*
+sed -i -e '/^DNS[0-9]*=.*/d' /etc/sysconfig/network-scripts/ifcfg-*
+
+# Remove known machine specific udev rules.
+find /etc/udev/rules.d/ -iname '70*net*' |xargs unlink
+
+# Prevent ntp from panicing from tinkering
+[ -a /etc/ntp.conf ] && \
+	grep -c 'tinker panic 0' /etc/ntp.conf || \
+	sed -i -e '1 i\tinker panic 0\n' /etc/ntp.conf
+
+>/root/.bash_history
+>/home/*/.bash_history
+>/root/anaconda-ks.cfg
+
+cat /dev/null > /var/log/wtmp
+
+sys-unconfig
+
