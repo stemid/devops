3db18b34ee0631ef2da38ff2f0c272e95edc2aa1
Stefan Midjich <swehack@gmail.com>
2016-10-26 11:23:56 +0200

    fixed configparser for python2.

--- nagios/check_vcenter_datastores.py
+++ nagios/check_vcenter_datastores.py
@@ -19,7 +19,10 @@ import readline
 from sys import exit, stderr
 from fnmatch import fnmatch
 from argparse import ArgumentParser, FileType
-from configparser import RawConfigParser as ConfigParser
+try:
+    from configparser import RawConfigParser as ConfigParser
+except ImportError:
+    from ConfigParser import RawConfigParser as ConfigParser
 from pprint import pprint
 from operator import itemgetter as i
 from functools import cmp_to_key
