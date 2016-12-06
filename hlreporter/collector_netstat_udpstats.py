80edabefb3c80f17d81cb65b632128284de0bd35
Stefan Midjich <swehack@gmail.com>
2016-12-06 21:40:29 +0100

    new collector for netstat -su

---
+++ hlreporter/collector_netstat_udpstats.py
@@ -0,0 +1,82 @@
+"""
+Collect stats from netstat -su. Requires sh module installed.
+"""
+
+from pprint import pprint as pp
+from re import match
+from io import StringIO
+
+can_collect = True
+
+try:
+    from sh import netstat
+except ImportError:
+    can_collect = False
+
+from hlreporterlib import collector
+from hlreporterlib import errors
+
+
+class NetstatUDPCollector(collector.BaseCollector):
+    name = 'netstat UDP stats'
+    platforms = ['linux']
+
+    def canCollect(self):
+        return can_collect
+
+
+    def collect(self):
+        self.hw = self.device.initComponentGroup('hardware')
+        self.net = self.hw.initComponentGroup('network')
+        self.root = self.net.initComponentGroup('netstat')
+        component = self.root.initComponent('udpstats')
+
+        udp_data = self.run_netstat()
+        aggr = []
+        pp(udp_data)
+
+        for data in udp_data:
+            out = component.initMetric(
+                data['key'],
+                int(data['value']),
+                'NUM_COUNTER',
+                data['key']+'/s',
+                value_per_interval=1
+            )
+            aggr.append(out)
+
+        component.initMetricGraph('udp', aggr)
+
+
+    def run_netstat(self):
+        buf = StringIO()
+        netstat('-su', _out=buf)
+        buf.seek(0)
+        data = []
+
+        donot_parse = True
+        for line in buf:
+            line = line.rstrip('\n')
+
+            if line.startswith('Udp:'):
+                donot_parse = False
+                continue
+
+            if line.startswith('UdpLite:'):
+                donot_parse = True
+
+            if donot_parse:
+                continue
+
+            pp(line)
+            m = match(r'^\s*([0-9]+)\b([^$]+)$', line)
+            if not m:
+                continue
+
+            data.append({
+                'key': m.group(2),
+                'value': m.group(1)
+            })
+
+        return data
+
